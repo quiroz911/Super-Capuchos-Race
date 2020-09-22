142874b845067e0fdc52b6ee30e991ba477dd9a3
Juan Pablo Quiroz Piedrahita <jpablo.quiroz@udea.edu.co>
2020-09-21 20:31:59 -0500

    primer commit

---
+++ Assets/Scripts/EnemiesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+    bool avoid = false;
+    string alphabet = "qwertyuiopasdfghjklzxcvbnm";
+    string avoidLetter;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        avoidLetter = alphabet.Substring(Random.Range(0, 27), 1);
+        Debug.Log(avoidLetter);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(avoid == true)
+        {
+            Destroy(gameObject);
+        }
+
+        gameObject.transform.Translate(-65f * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(avoidLetter))
+        {
+            avoid = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        InterfazDeUsuario interfazDeUsuario = FindObjectOfType<InterfazDeUsuario>();
+        interfazDeUsuario.MostrarMensajeSalteableTecla("Game over, presione cualquier tecla para continuar");
+        Destroy(gameObject);
+    }
+
+   
+}
---
+++ Assets/UI/Mostrar Mensajes en Pantalla - GameDevTraum/InterfazDeUsuario.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfazDeUsuario : MonoBehaviour
+{
+
+    #region CAMPOS
+
+    [Header("Mensaje Salteable (cualquier tecla)")]
+
+
+    [SerializeField]
+    private GameObject mensajeSalteableTeclaObjeto; //Contenedor
+    [SerializeField]
+    private Text mensajeSalteableTeclaTexto; //Objeto Texto
+    private bool mensajeSalteableTeclaActivo; //Estado del mensaje
+
+
+    #endregion
+
+    #region MÉTODOS
+
+
+    void Start()
+    {
+        mensajeSalteableTeclaObjeto.SetActive(false);
+    }
+
+
+    private void OnGUI()
+    {
+        if (mensajeSalteableTeclaActivo)
+        {
+            if (Input.anyKeyDown)
+            {
+                LimpiarMensajeTecla();
+            }
+        }
+
+    }
+
+    public void MostrarMensajeSalteableTecla(string mensaje)
+    {
+        mensajeSalteableTeclaActivo = true;
+        mensajeSalteableTeclaTexto.text = mensaje;
+        mensajeSalteableTeclaObjeto.SetActive(true);
+    }
+
+    private void LimpiarMensajeTecla()
+    {
+        mensajeSalteableTeclaActivo = false;
+        mensajeSalteableTeclaObjeto.SetActive(false);
+    }
+
+    
+
+    #endregion
+
+}
